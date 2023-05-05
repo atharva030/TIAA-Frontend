d59e7f833d34e8f00c6315093188c50dcec331ff
Devidpatle <96178882+Devidpatle@users.noreply.github.com>
2023-05-06 01:17:29 +0530

    New Commit-form

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -2,173 +2,18 @@ import "./list.css";
 import Navbar from "../../components/navbar/Navbar";
 import Header from "../../components/header/Header";
 import { useLocation } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { format } from "date-fns";
 import { DateRange } from "react-date-range";
 import SearchItem from "../../components/searchItem/SearchItem";
+import Multiselect from 'multiselect-react-dropdown';
 
 const List = () => {
-  const [status, setStatus] = useState([
-    {
-      "_id": "64540a0ea2ff2ed7f01352b5",
-      "organisationName": "Visnupuri Ration Org",
-      "rationType": [
-        "Orange"
-      ],
-      "rationDetails": [
-        "Type"
-      ],
-      "rationSchedule": "11:00-15:00",
-      "rationSlots": "10",
-      "rating":"8.5",
-      "rationStock": "Type",
-      "rationProvider": "Daily Use",
-      "rationMfg": "3/22",
-      "rationExp": "3/24",
-      "location": "Visnupuri Temple",
-      "eventDate": "21-02-22",
-      "date": "2023-05-04T19:39:58.176Z",
-    },
-    {
-      "_id": "64540a18a2ff2ed7f01352b7",
-      "organisationName": "atharva",
-      "rationType": [
-        "Blue"
-      ],
-      "rationDetails": [
-        "Type"
-      ],
-      "rationSchedule": "Type",
-      "rationSlots": "10",
-      "rating":"8.5",
-      "rationStock": "Type",
-      "rationProvider": "Type",
-      "rationMfg": "Type",
-      "rationExp": "Type",
-      "location": "Type",
-      "eventDate": "25-02-22",
-      "date": "2023-05-04T19:40:08.530Z",
-    },
-    {
-      "_id": "64540a27a2ff2ed7f01352b9",
-      "organisationName": "atharva",
-      "rationType": [
-        "Green"
-      ],
-      "rationDetails": [
-        "Type"
-      ],
-      "rationSchedule": "Type",
-      "rationSlots": "10",
-      "rating":"8.5",
-      "rationStock": "Type",
-      "rationProvider": "Type",
-      "rationMfg": "Type",
-      "rationExp": "Type",
-      "location": "Type",
-      "eventDate": "29-02-23",
-      "date": "2023-05-04T19:40:23.913Z",
-    },
-    {
-      "_id": "6454ccf87415081c8a30c1d3",
-      "organisationName": "atharva",
-      "rationType": [
-        "Green"
-      ],
-      "rationDetails": [
-        "Type",
-        "TYPE@"
-      ],
-      "rationSchedule": "Type",
-      "rationSlots": "10",
-      "rating":"8.5",
-      "rationStock": "Type",
-      "rationProvider": "Type",
-      "rationMfg": "Type",
-      "rationExp": "Type",
-      "location": "Type",
-      "eventDate": "29-02-23",
-      "date": "2023-05-05T09:31:36.600Z",
-    },
-    {
-      "_id": "6454cd88dcf326b803bd10b9",
-      "organisationName": "atharva",
-      "rationType": [
-        "Green",
-        "Yellow"
-      ],
-      "rationDetails": [
-        "Type",
-        "TYPE@"
-      ],
-      "rationSchedule": "Type",
-      "rationSlots": "10",
-      "rating":"8.5",
-      "rationStock": "Type",
-      "rationProvider": "Type",
-      "rationMfg": "Type",
-      "rationExp": "Type",
-      "location": "Type",
-      "eventDate": "29-02-23",
-      "date": "2023-05-05T09:34:00.955Z",
-    },
-    {
-      "_id": "6454d09bd06d4bdb770b4e56",
-      "user": "6453f987780a294a21c98a35",
-      "organisationName": "atharva",
-      "rationType": [
-        "Green",
-        "Yellow"
-      ],
-      "rationDetails": [
-        "Type",
-        "TYPE@"
-      ],
-      "rationSchedule": "Type",
-      "rationSlots": "10",
-      "rating":"8.5",
-      "rationStock": "Type",
-      "rationProvider": "Type",
-      "rationMfg": "Type",
-      "rationExp": "Type",
-      "location": "Type",
-      "eventDate": "29-02-23",
-      "date": "2023-05-05T09:47:07.099Z",
-    },
-    {
-      "_id": "6454e32c0bd898bb01f9aeb7",
-      "user": "6453f987780a294a21c98a35",
-      "organisationName": "atharva",
-      "rationType": [
-        "Green",
-        "Yellow"
-      ],
-      "rationDetails": [
-        "Type",
-        "TYPE@"
-      ],
-      "rationSchedule": "Type",
-      "rationSlots": "10",
-      "rating":"8.5",
-      "rationStock": "Type",
-      "rationProvider": "Type",
-      "rationMfg": "Type",
-      "rationExp": "Type",
-      "location": "Type",
-      "eventDate": "29-02-23",
-      "date": "2023-05-05T11:06:20.602Z",
-    }
-  ])
   const location = useLocation();
   // const [destination, setDestination] = useState(location.state.destination);
   // const [date, setDate] = useState(location.state.date);
   // const [openDate, setOpenDate] = useState(false);
   // const [options, setOptions] = useState(location.state.options);
-useEffect(() => {
-  console.log(status)
-
-  
-}, [])
 
   return (
     <div>
@@ -179,7 +24,7 @@ useEffect(() => {
           <div className="listSearch">
             <h1 className="lsTitle">Search</h1>
             <div className="lsItem">
-              <label>Destination</label>
+            <label>Destination</label>
               {/* <input placeholder={destination} type="text" /> */}
             </div>
             {/* <div className="lsItem">
@@ -197,88 +42,106 @@ useEffect(() => {
               )}
             </div> */}
             <div className="lsItem">
-              <label>Options</label>
               <div className="lsOptions">
                 <div className="lsOptionItem">
                   <span className="lsOptionText">
-                    Min price <small>per night</small>
+                    Ration Type 
                   </span>
-                  <input type="number" className="lsOptionInput" />
+                  {/* <input type="number" className="lsOptionInput" /> */}
+                  <Multiselect
+  isObject={false}
+  onKeyPressFn={function noRefCheck(){}}
+  onRemove={function noRefCheck(){}}
+  onSearch={function noRefCheck(){}}
+  onSelect={function noRefCheck(){}}
+  options={[
+    'Yellow Card',
+    'Orange Card',
+    
+  ]}
+/>
                 </div>
                 <div className="lsOptionItem">
                   <span className="lsOptionText">
-                    Max price <small>per night</small>
+                    Ration Details 
                   </span>
-                  <input type="number" className="lsOptionInput" />
+                  <Multiselect
+  isObject={false}
+  onKeyPressFn={function noRefCheck(){}}
+  onRemove={function noRefCheck(){}}
+  onSearch={function noRefCheck(){}}
+  onSelect={function noRefCheck(){}}
+  options={[
+    'Wheat',
+    'Rice',
+    'Sugar',
+    'Oil',
+    'Tarmaric',
+  ]}
+/>
                 </div>
                 <div className="lsOptionItem">
-                  <span className="lsOptionText">Adult</span>
-                  <input
-                    type="number"
-                    min={1}
-                    className="lsOptionInput"
-                    // placeholder={options.adult}
-                  />
+                  <span className="lsOptionText">Ration Schedule</span>
+                  <input type="datetime-local" name="" id="" />
                 </div>
                 <div className="lsOptionItem">
-                  <span className="lsOptionText">Children</span>
-                  <input
-                    type="number"
-                    min={0}
-                    className="lsOptionInput"
-                    // placeholder={options.children}
-                  />
+                  <span className="lsOptionText">Ration Allocated </span>
+                  <Multiselect
+  isObject={false}
+  onKeyPressFn={function noRefCheck(){}}
+  onRemove={function noRefCheck(){}}
+  onSearch={function noRefCheck(){}}
+  onSelect={function noRefCheck(){}}
+  options={[
+    '2 Kg',
+    '5 kg',
+    '10 kg',
+    '15 kg',
+  ]}
+/>
                 </div>
                 <div className="lsOptionItem">
-                  <span className="lsOptionText">Room</span>
-                  <input
-                    type="number"
-                    min={1}
-                    className="lsOptionInput"
-                    // placeholder={options.room}
-                  />
+                  <span className="lsOptionText">Ration Stock</span>
+                  <Multiselect
+  isObject={false}
+  onKeyPressFn={function noRefCheck(){}}
+  onRemove={function noRefCheck(){}}
+  onSearch={function noRefCheck(){}}
+  onSelect={function noRefCheck(){}}
+  options={[
+    'Less than 100 Kg',
+    '200+ Kg',
+    '500+ Kg'
+    
+    
+  ]}
+/>
+                </div>
+                <div className="lsOptionItem">
+                  <span className="lsOptionText">Ration Provider</span>
+                  <Multiselect
+  isObject={false}
+  onKeyPressFn={function noRefCheck(){}}
+  onRemove={function noRefCheck(){}}
+  onSearch={function noRefCheck(){}}
+  onSelect={function noRefCheck(){}}
+  options={[
+    'Adwait Sharma',
+    'Nagpur'
+    
+  ]}
+/>
+                </div>
+                <div className="lsOptionItem">
+                  <span className="lsOptionText">Ration Mfg/Exp</span>
+                  <input type="datetime-local" name="" id="" />
                 </div>
               </div>
             </div>
             <button>Search</button>
           </div>
           <div className="listResult">
-            {/* <SearchItem 
-            organisationName="Visnupuri Ration Org" 
-            rationType="Yellow" 
-            rationSchedule= "11:00-15:00" 
-            rationProvider= "Nanded Daily Use Store"
-            eventDate= "6-05-2023"
-            rationAllocate=""
-            rationSlots= "10"
-            rationMfg="Type"
-            rationExp="Type"
-            location= "Type"
-            date="2023-05-04T19:39:58.176Z"
-            /> */}
-            {status.length === 0 ? (
-                        (
-
-                           <div className="siFeatures">NO EVENTS FOUND</div>
-                        )
-                    ) : (
-                        status.map((items) => (
-                            <SearchItem
-                            organisationName={items.organisationName} 
-                            rationType={items.rationType} 
-                            rationSchedule= {items.rationSchedule} 
-                            rationProvider= {items.rationProvider}
-                            eventDate= {items.eventDate}
-                            rationAllocate={items.rationAllocate}
-                            rationSlots= {items.rationSlots}
-                            rationMfg={items.rationMfg}
-                            rationExp={items.rationExp}
-                            location= {items.location}
-                            date={items.date}
-                            rating={items.rating}
-                            />
-                        ))
-                    )}
+            <SearchItem organisationName="organisationName" />
             {/* <SearchItem />
             <SearchItem />
             <SearchItem />
