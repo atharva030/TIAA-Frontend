f41204c642f1f2d6336fdce3b8b9b59a2aeadab5
Devidpatle <96178882+Devidpatle@users.noreply.github.com>
2023-05-06 15:25:38 +0530

    New Commit

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -6,17 +6,26 @@ import { useState } from "react";
 import { format } from "date-fns";
 import { DateRange } from "react-date-range";
 import SearchItem from "../../components/searchItem/SearchItem";
-
-import Multiselect from 'multiselect-react-dropdown';
-
-
-
+import Multiselect from "multiselect-react-dropdown";
 
 const List = () => {
-
-
-
   const location = useLocation();
+  const [formData, setFormData] = useState({
+    "rationType":[],
+    "rationDetails":[],
+    "rationShedule":"",
+    "rationAlloted":[],
+    "rationStock":[],
+    "rationProvider":[],
+    "rationExp":"",
+  })
+
+
+ const handleSubmit = async(e)=>{
+  e.preventDefault()
+  console.log(formData)
+ }
+
   // const [destination, setDestination] = useState(location.state.destination);
   // const [date, setDate] = useState(location.state.date);
   // const [openDate, setOpenDate] = useState(false);
@@ -24,13 +33,11 @@ const List = () => {
 
   return (
     <div>
-
-
       <Navbar />
-      <Header  />
+      <Header />
       <div className="listContainer">
         <div className="listWrapper">
-          <div className="listSearch">
+          <form className="listSearch" onSubmit={handleSubmit}>
             <h3 className="lsTitle">Create Your Organization</h3>
             <div className="lsItem">
               <label>Destination</label>
@@ -53,102 +60,77 @@ const List = () => {
             <div className="lsItem">
               <div className="lsOptions">
                 <div className="lsOptionItem">
-                  <span className="lsOptionText">
-                    Ration Type 
-                  </span>
+                  <span className="lsOptionText">Ration Type</span>
                   {/* <input type="number" className="lsOptionInput" /> */}
                   <Multiselect
-  isObject={false}
-  onKeyPressFn={function noRefCheck(){}}
-  onRemove={function noRefCheck(){}}
-  onSearch={function noRefCheck(){}}
-  onSelect={function noRefCheck(){}}
-  options={[
-    'Yellow Card',
-    'Orange Card',
-    
-  ]}
-/>
+                  
+                    isObject={false}
+                    onKeyPressFn={function noRefCheck() {}}
+                    onRemove={(e)=>{setFormData({...formData, rationType:e})}}
+                    onSearch={function noRefCheck() {}}
+                    onSelect={(e)=>{setFormData({...formData, rationType:e})}}
+                    required
+                    options={["Yellow Card", "Orange Card"]}
+                    
+                  />
                 </div>
                 <div className="lsOptionItem">
-                  <span className="lsOptionText">
-                    Ration Details 
-                  </span>
+                  <span className="lsOptionText">Ration Details</span>
                   <Multiselect
-  isObject={false}
-  onKeyPressFn={function noRefCheck(){}}
-  onRemove={function noRefCheck(){}}
-  onSearch={function noRefCheck(){}}
-  onSelect={function noRefCheck(){}}
-  options={[
-    'Wheat',
-    'Rice',
-    'Sugar',
-    'Oil',
-    'Tarmaric',
-  ]}
-/>
+                    isObject={false}
+                    onKeyPressFn={function noRefCheck() {}}
+                    onRemove={(e)=>{setFormData({...formData, rationDetails:e})}}
+                    onSearch={function noRefCheck() {}}
+                    onSelect={(e)=>{setFormData({...formData, rationDetails:e})}}
+                    options={["Wheat", "Rice", "Sugar", "Oil", "Tarmaric"]}
+                  />
                 </div>
                 <div className="lsOptionItem">
                   <span className="lsOptionText">Ration Schedule</span>
-                  <input type="datetime-local" name="" id="" />
+                  <input type="datetime-local" onChange={(e)=>{setFormData({...formData, rationShedule:e.target.value})}} name="" id="" />
                 </div>
                 <div className="lsOptionItem">
                   <span className="lsOptionText">Ration Allocated </span>
                   <Multiselect
-  isObject={false}
-  onKeyPressFn={function noRefCheck(){}}
-  onRemove={function noRefCheck(){}}
-  onSearch={function noRefCheck(){}}
-  onSelect={function noRefCheck(){}}
-  options={[
-    '2 Kg',
-    '5 kg',
-    '10 kg',
-    '15 kg',
-  ]}
-/>
+                    isObject={false}
+                    onKeyPressFn={function noRefCheck() {}}
+                    onRemove={(e)=>{setFormData({...formData, rationAllotted:e})}}
+                    onSearch={function noRefCheck() {}}
+                    onSelect={(e)=>{setFormData({...formData, rationAlloted:e})}}
+                    options={["2 Kg", "5 kg", "10 kg", "15 kg"]}
+                  />
                 </div>
                 <div className="lsOptionItem">
                   <span className="lsOptionText">Ration Stock</span>
                   <Multiselect
-  isObject={false}
-  onKeyPressFn={function noRefCheck(){}}
-  onRemove={function noRefCheck(){}}
-  onSearch={function noRefCheck(){}}
-  onSelect={function noRefCheck(){}}
-  options={[
-    'Less than 100 Kg',
-    '200+ Kg',
-    '500+ Kg'
-    
-    
-  ]}
-/>
+                    isObject={false}
+                    onKeyPressFn={function noRefCheck() {}}
+                    onRemove={(e)=>{setFormData({...formData, rationStock:e})}}
+                    onSearch={function noRefCheck() {}}
+                    onSelect={(e)=>{setFormData({...formData, rationStock:e})}}
+                    options={["Less than 100 Kg", "200+ Kg", "500+ Kg"]}
+                  />
                 </div>
                 <div className="lsOptionItem">
                   <span className="lsOptionText">Ration Provider</span>
                   <Multiselect
-  isObject={false}
-  onKeyPressFn={function noRefCheck(){}}
-  onRemove={function noRefCheck(){}}
-  onSearch={function noRefCheck(){}}
-  onSelect={function noRefCheck(){}}
-  options={[
-    'Adwait Sharma',
-    'Nagpur'
-    
-  ]}
-/>
+                    isObject={false}
+                    // onKeyPressFn={(e)=>{console.log(e)}}
+                    onRemove={(e)=>{setFormData({...formData, rationProvider:e})}}
+                    onSearch={function noRefCheck() {}}
+                    // onSelect={function noRefCheck() {}}
+                    options={["Adwait Sharma", "Nagpur"]}
+                    onSelect={(e)=>{setFormData({...formData, rationProvider:e})}}
+                  />
                 </div>
                 <div className="lsOptionItem">
                   <span className="lsOptionText">Ration Mfg/Exp</span>
-                  <input type="datetime-local" name="" id="" />
+                  <input type="datetime-local" onChange={(e)=>{setFormData({...formData, rationExp:e.target.value})}} name="" id="" />
                 </div>
               </div>
             </div>
-            <button>Search</button>
-          </div>
+            <button type="submit">Search</button>
+          </form>
           <div className="listResult">
             <SearchItem organisationName="organisationName" />
             {/* <SearchItem />
